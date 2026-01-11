763e229b89ac6724f5c57a573650aed8ca505020
LvzBxLzSyP <aa0913570907@gmail.com>
2026-01-11 15:55:59 +0800

    well a little change of the fucking const

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (file of commandFiles) {
+    for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
         if ('data' in command && 'execute' in command) {
